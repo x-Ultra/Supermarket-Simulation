483ce43dd705554516cd45b2acafc6115cccbe3b
Caporaso Pasquale <caporaso_pasquale@libero.it>
2020-08-18 13:24:38 +0200

    Added more graphs

--- specification_model/analyze.py
+++ specification_model/analyze.py
@@ -4,7 +4,10 @@ import csv
 import scipy.stats as st
 import re
 from datetime import datetime
+from sklearn import datasets, linear_model
 
+
+monday = "2017-12-11"
 
 def days_between(d1, d2):
     d1 = datetime.strptime(d1, "%Y-%m-%d")
@@ -37,7 +40,8 @@ def get_best_distribution(data):
 
 
 
-def plot_trans_time():
+
+def plot_trans_time_old():
 	fd = open('trans_time.csv', newline='')
 	trans_time = csv.reader(fd, delimiter=',', quotechar='"')
 
@@ -61,15 +65,71 @@ def plot_trans_time():
 				pass
 		
 
+
+	best_fit = np.polyfit(items_1, times_1, 1)
+	# Train the model using the training set
+
+	# Make predictions using the testing set
+
 	#for i in range(0, len(items)):
 	#	print(times[i], items[i])
 
 	plt.scatter(items_1, times_1, s=5, color="r")
-	plt.scatter(items_8, times_8, s=5, color="b")
+	#plt.scatter(items_8, times_8, s=5, color="b")
 
+	pred_1 = [best_fit[0]*item + best_fit[1] for item in items_1]
+
+	plt.plot(items_1, pred_1 , color="b")
+	plt.xlabel("Number of items")
+	plt.ylabel("Duration of transaction")
 	plt.show()
 
-def plot_item_quantity():
+def plot_trans_time(day_filter):
+	fd = open('trans_time.csv', newline='')
+	trans_time = csv.reader(fd, delimiter=',', quotechar='"')
+
+	times_1 = []
+	items_1 = []
+
+	times_8 = []
+	items_8 = []
+	for row in trans_time:
+
+		if row[0] == 'WorkstationGroupID':
+			continue
+		day = row[2].split("T")[0]
+
+		if days_between(day, monday) % 7 == day_filter:
+			if row[0] == '1':
+				times_1.append(int(row[5]))
+				items_1.append(int(row[7]))
+			else:
+				times_8.append(int(row[5]))
+				items_8.append(int(row[7]))
+			
+
+
+	best_fit = np.polyfit(items_1, times_1, 1)
+	# Train the model using the training set
+
+	# Make predictions using the testing set
+
+	#for i in range(0, len(items)):
+	#	print(times[i], items[i])
+
+	plt.scatter(items_1, times_1, s=5, color="r")
+	#plt.scatter(items_8, times_8, s=5, color="b")
+
+	pred_1 = [best_fit[0]*item + best_fit[1] for item in items_1]
+
+	plt.plot(items_1, pred_1 , color="b")
+	plt.xlabel("Number of items for day " + str(day_filter))
+	plt.ylabel("Duration of transaction")
+	#plt.show()
+	plt.savefig("serve_speed_" + str(day_filter) + ".png")
+	plt.close()
+
+def plot_item_quantity_old():
 	fd = open('item_quantity.csv', newline='')
 	item_quantity = csv.reader(fd, delimiter=',', quotechar='"')
 
@@ -83,8 +143,41 @@ def plot_item_quantity():
 			pass
 	plt.plot(items, quantity)
 	
+	plt.xlabel("Number of items")
+	plt.ylabel("Number of transactions")
+
 	plt.show()
 
+	#get_best_distribution(quantity)
+
+def plot_item_quantity(day_filter):
+	fd = open('trans_time.csv', newline='')
+	item_quantity = csv.reader(fd, delimiter=',', quotechar='"')
+
+	items = [ i for i in range(1, 300)]
+	quantity = [0]*299
+
+	for row in item_quantity:
+
+		if row[0] == 'WorkstationGroupID':
+			continue
+
+		day = row[2].split("T")[0]
+		
+		#print(day, monday)
+		if day_filter == None or days_between(day, monday) % 7 == day_filter:
+			#print(int(row[7])-1)
+			if int(row[7])-1 < 299:
+				quantity[int(row[7])-1] += 1
+
+	plt.plot(items, quantity)
+	
+	plt.xlabel("Number of items for day " + str(day_filter))
+	plt.ylabel("Number of transactions")
+
+	#plt.show()
+	plt.savefig('item_quantity_'+str(day_filter)+'.png')
+	plt.close()
 	#get_best_distribution(quantity)
 
 
@@ -119,7 +212,7 @@ def find_full_days():
 
 	return full_days
 
-def plot_hour_transaction():
+def plot_hour_transaction_old():
 	fd = open('trans_time.csv', newline='')
 	trans_time = csv.reader(fd, delimiter=',', quotechar='"')
 
@@ -158,8 +251,65 @@ def plot_hour_transaction():
 	print(ratio)
 	plt.bar(hours, ratio)
 	
+
+	plt.xlabel("Hour of the day")
+	plt.ylabel("Average number of transactions")
+
 	plt.show()
 
+def plot_hour_transaction(day_filter):
+	fd = open('trans_time.csv', newline='')
+	trans_time = csv.reader(fd, delimiter=',', quotechar='"')
+
+
+	monday = "2017-12-11"
+	hours = [i for i in range(0, 24)]
+	trans = [0]*24
+	curr_hour = 0
+	total_hours = [0]*24
+
+	for row in trans_time:
+		if row[0] == 'WorkstationGroupID':
+			continue
+		day = row[2].split("T")[0]
+		hour = row[2].split("T")[1]
+
+		if curr_hour == 0:
+			curr_hour = int(hour.split(":")[0])
+
+
+		if days_between(day, monday) % 7 == day_filter:
+			hour = int(hour.split(":")[0])
+
+			if curr_hour != hour:
+				total_hours[curr_hour] += 1
+				curr_hour = hour
+
+			trans[hour] += 1
+
+	
+	ratio = [0]*24
+	for i in range(0, len(total_hours)):
+		if total_hours[i] == 0:
+			ratio[i] = 0
+		else:
+			ratio[i] = trans[i]/total_hours[i]
+
+	for i in range(6, len(ratio), 2):
+		#print(float(ratio[i]), float(ratio[i+1]), (float(ratio[i])+float(ratio[i+1])))
+		print("%.2f" % (float((float(ratio[i])+float(ratio[i+1])))/2), end=" & ")
+	print("")
+	plt.bar(hours, ratio)
+	
+
+	plt.xlabel("Hour of the day " + str(day_filter))
+	plt.ylabel("Average number of transactions")
+
+	#plt.show()
+
+	plt.savefig('transtaction_per_hour_normalized_'+str(day_filter)+'.png')
+
+	plt.close()
 
 def plot_day_transaction():
 
@@ -200,7 +350,42 @@ def plot_day_transaction():
 	print(ratio)
 	plt.bar(days, ratio)
 	
+	plt.xlabel("Day of the week")
+	plt.ylabel("Average number of transactions per hour")
 	plt.show()	
 
 
-plot_day_transaction()
+for i in range(6, 23):
+	print(str(i)+":00 &", end=' ')
+print("")
+for i in range(0,7):
+	plot_hour_transaction(i)
+
+from math import exp,log10
+from scipy.stats import norm
+import random
+
+mu = 24/60/60
+
+#print(mu)
+
+#values = [-mu*log10(1-random.random()) for i in range(0, 1000)]
+values = [random.expovariate(mu) for i in range(0, 10000)]
+
+#print(values)
+
+count = 0
+average = 0
+time_passed = 0
+total = 0
+for i in range(0, len(values)):
+	time_passed += values[i]
+	count += 1
+	
+	if time_passed > 3600:
+		time_passed = time_passed - 3600
+		average += count
+		count = 0
+		total += 1
+
+print(average / total)
